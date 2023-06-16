81259691522bddf54b4dad336eaf21330c1c7737
AaravSonthalia <136267688+AaravSonthalia@users.noreply.github.com>
2023-06-16 11:01:13 -0400

    Add files via upload

---
+++ madlib-project.js
@@ -0,0 +1,22 @@
+// creating variables
+let name = "Jack";
+let sport = "Squidly";
+const team1 = "Spoons";
+const team2 = "Forks";
+let score1 = 173;
+let score2 = 25;
+let adjective1 = "disgusting";
+let adjective2 = "dumb";
+let reason = "he stubbed his toe";
+let status = true;
+
+// creating the story
+let story = "Yesterday me and " + name + " went to see a " + sport + " game. ";
+story += "The teams playing were the " + team1 + " and the " + team2 + ". ";
+story += "The score was " + score1 + " to " + score2 + " and the " + team1 + " team won! ";
+story += "The game was very " + adjective1 + " and the players were very " + adjective2 + ". ";
+story += name + " did not like the game because " + reason + ". ";
+story += status + " story!";
+
+// printing to console
+console.log(story);
